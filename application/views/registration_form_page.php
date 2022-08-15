9554c680e772cbaf5c150ac10ec7fcfc1889e3cd
Aurko Home <mahmudur.rashid@northsouth.edu>
2022-08-15 16:13:38 +0600

    configured and registerestration view page and model page added

---
+++ application/views/registration_form_page.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Page</title>
+</head>
+<body>
+<h1>Register Here</h1>
+
+<form method="POST" role="form">
+	<input type="text" name="name" placeholder="Enter your name"><br>
+	<input type="email" name="email" placeholder="Enter your email"><br>
+	<input type="password" name="password" placeholder="Enter your password"><br>
+	<button type="submit" name="register_here">Register here</button>
+</form>
+</body>
+</html>
